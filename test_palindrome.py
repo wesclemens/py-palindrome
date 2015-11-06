f336847ffb046371e6dc83467eb0dac0afc0bd64
William Clemens <wesclemens@gmail.com>
2015-11-06 14:54:35 -0600

    Added negative testes

--- test_palindrome.py
+++ test_palindrome.py
@@ -2,10 +2,14 @@ import unittest
 from palindrome import checker
 
 class TestPalindrome(unittest.TestCase):
-    def test_even(self):
+    def test_even_true(self):
         self.assertTrue(checker("abccba"))
-    def test_odd(self):
+    def test_odd_true(self):
         self.assertTrue(checker("abcdcba"))
+    def test_even_false(self):
+        self.assertFalse(checker("abcd"))
+    def test_odd_false(self):
+        self.assertFalse(checker("abcde"))
 
 if '__main__' == __name__:
     unittest.main()
